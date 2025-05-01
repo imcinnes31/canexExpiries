abb1611cc0d274e2d42ede7b7aafb47204cd1993
imcinnes31 <imcinnes31@gmail.com>
2025-05-01 01:35:58 -0500

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,14 @@
+import { NavLink, useLocation } from "react-router-dom";
+
 export default function Footer() {
     return (
-      <div className="text-xl font-bold text-center font-serif pt-10 print:hidden">
-        ©2025 Ian McInnes, "Shock" Productions
+      <div>
+        <div className="flex flex-row justify-center print:hidden">      
+          {useLocation().pathname == "/" ? null : <NavLink className="text-xl underline decoration-blue-400 text-blue-400" to="/">Main Menu</NavLink>}
+        </div>
+        <div className="text-xl font-bold text-center font-serif pt-10 print:hidden">
+          ©2025 Ian McInnes, "Shock" Productions
+        </div>
       </div>
     );
   }
